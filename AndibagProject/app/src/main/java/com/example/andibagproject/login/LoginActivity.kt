6c3fc462ce2183a0699eb8affc04667f4389b163
kimjjunho <kimjjun1007@gmail.com>
2022-05-17 08:27:00 +0900

    init::LoginActivity

---
+++ AndibagProject/app/src/main/java/com/example/andibagproject/login/LoginActivity.kt
@@ -0,0 +1,14 @@
+package com.example.andibagproject.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.andibagproject.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+
+    }
+}
