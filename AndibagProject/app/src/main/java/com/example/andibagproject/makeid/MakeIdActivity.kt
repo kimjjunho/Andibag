b3b02a52afce7a36c484b04455af3c65ca318794
kimjjunho <kimjjun1007@gmail.com>
2022-05-17 09:13:01 +0900

    init::MakeIdActivity

---
+++ AndibagProject/app/src/main/java/com/example/andibagproject/makeid/MakeIdActivity.kt
@@ -0,0 +1,12 @@
+package com.example.andibagproject.makeid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.andibagproject.R
+
+class MakeIdActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_make_id)
+    }
+}
