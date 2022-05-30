63e731c630294f2ba3931d46ec53c216b3a70114
kimjjunho <kimjjun1007@gmail.com>
2022-05-30 09:28:30 +0900

    init::Move To SearchActivity
    FriendFragment -> SearchActivity 로 이동

--- AndibagProject/app/src/main/java/com/example/andibagproject/friend/FriendFragment.kt
+++ AndibagProject/app/src/main/java/com/example/andibagproject/friend/FriendFragment.kt
@@ -1,23 +1,19 @@
 package com.example.andibagproject.friend
-
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.content.Intent
+import com.example.andibagproject.MainActivity
 import com.example.andibagproject.R
+import com.example.andibagproject.base.BaseFragment
+import com.example.andibagproject.databinding.FragmentFriendBinding
 
-class FriendFragment : Fragment() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+class FriendFragment : BaseFragment<FragmentFriendBinding>(R.layout.fragment_friend) {
+    override fun initView() {
+        val context = activity as MainActivity
+        binding.run {
+            imageSearch.setOnClickListener {
+                startActivity(Intent(context,SearchActivity()::class.java))
+            }
+        }
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-
-        return inflater.inflate(R.layout.fragment_friend, container, false)
-    }
-
-    companion object {
-    }
+    override fun observeEvent() {}
 }
