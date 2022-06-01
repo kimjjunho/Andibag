9915d87d247a8a047ffdd3027422616ab6498933
kimjjunho <kimjjun1007@gmail.com>
2022-06-02 08:38:46 +0900

    fix Ui

--- AndibagProject/app/src/main/java/com/example/andibagproject/friend/AddActivity.kt
+++ AndibagProject/app/src/main/java/com/example/andibagproject/friend/AddActivity.kt
@@ -3,11 +3,21 @@ package com.example.andibagproject.friend
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.andibagproject.R
+import com.example.andibagproject.databinding.ActivityAddBinding
+import com.example.andibagproject.databinding.ActivityLoginBinding
 
 class AddActivity : AppCompatActivity() {
+
+    private lateinit var mBinding : ActivityAddBinding
+    private val binding get() = mBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add)
 
+        mBinding = ActivityAddBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.imageBack.setOnClickListener {
+            finish()
+        }
     }
 }
--- AndibagProject/app/src/main/java/com/example/andibagproject/friend/SearchActivity.kt
+++ AndibagProject/app/src/main/java/com/example/andibagproject/friend/SearchActivity.kt
@@ -2,11 +2,20 @@ package com.example.andibagproject.friend
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.andibagproject.R
+import com.example.andibagproject.databinding.ActivitySearchBinding
 
 class SearchActivity : AppCompatActivity() {
+
+    private lateinit var mBinding : ActivitySearchBinding
+    private val binding get() = mBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_search)
+
+        mBinding = ActivitySearchBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.imageBack.setOnClickListener {
+            finish()
+        }
     }
 }
