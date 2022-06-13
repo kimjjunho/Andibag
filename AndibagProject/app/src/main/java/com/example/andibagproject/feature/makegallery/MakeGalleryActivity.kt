8efdec5600b65b2cd6a626eebe0b02903c44aa9c
kimjjunho <kimjjun1007@gmail.com>
2022-06-13 22:27:30 +0900

    init::MakeGalleryActivity

---
+++ AndibagProject/app/src/main/java/com/example/andibagproject/feature/makegallery/MakeGalleryActivity.kt
@@ -0,0 +1,14 @@
+package com.example.andibagproject.feature.makegallery
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.andibagproject.R
+
+class MakeGalleryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_make_gallery)
+
+
+    }
+}
