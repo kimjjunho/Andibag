6c2ebb497e226329511447dcd325002a54109a61
kimjjunho <kimjjun1007@gmail.com>
2022-06-14 21:04:03 +0900

    서버가 닫혀있을 때 뜨는 오류 해결

--- AndibagProject/app/src/main/java/com/example/andibagproject/feature/login/viewmodel/LoginViewModel.kt
+++ AndibagProject/app/src/main/java/com/example/andibagproject/feature/login/viewmodel/LoginViewModel.kt
@@ -2,19 +2,17 @@ package com.example.andibagproject.feature.login.viewmodel
 
 import android.content.ContentValues.TAG
 import android.util.Log
+import android.widget.Toast
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.andibagproject.ACCESS_TOKEN
 import com.example.andibagproject.Authority
 import com.example.andibagproject.REFRESH_TOKEN
 import com.example.andibagproject.data.login.LoginRepository
-import com.example.andibagproject.data.loginAPI
 import com.example.andibagproject.feature.login.model.LoginRequest
-import com.example.andibagproject.feature.login.model.LoginResponse
-import io.reactivex.rxjava3.annotations.NonNull
-import io.reactivex.rxjava3.core.Single
-import io.reactivex.rxjava3.schedulers.Schedulers
-import retrofit2.Response
+import io.reactivex.rxjava3.exceptions.OnErrorNotImplementedException
+import java.lang.Exception
+import java.net.ConnectException
 
 class LoginViewModel(
     private val rp: LoginRepository
@@ -22,21 +20,23 @@ class LoginViewModel(
     val success : MutableLiveData<Boolean> = MutableLiveData()
     val fail : MutableLiveData<Int> = MutableLiveData()
 
-    fun login(loginRequest: LoginRequest){
+    fun login(loginRequest: LoginRequest) {
         rp.login(loginRequest)
-            .doOnError { throwable ->
-                Log.d(TAG, "Error: $throwable")
-            }
-            .subscribe{ response->
-                if(response.isSuccessful){
+            .subscribe( { response->
+                Log.d(TAG, "login: try")
+                if (response.isSuccessful) {
                     ACCESS_TOKEN = "Bearer " + response.body()?.accessToken.toString()
-                    Log.d(TAG,"Bearer " + response.body()?.accessToken.toString())
+                    Log.d(TAG, "Bearer " + response.body()?.accessToken.toString())
                     REFRESH_TOKEN = response.body()?.refreshToken.toString()
                     Authority = response.body()?.Authority!!
                     success.value = true
-                }else{
+                } else {
                     fail.value = response.code()
                 }
-            }
+            }, {
+                when(it) {
+                    is ConnectException -> fail.value = 500
+                }
+            })
     }
 }
