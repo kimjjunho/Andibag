104225c74c0fb3eeddc239c112cf302078c241f6
kimjjunho <kimjjun1007@gmail.com>
2022-06-14 09:57:24 +0900

    init::SeeGalleryViewModel

--- AndibagProject/app/src/main/java/com/example/andibagproject/feature/gallery/viewmodel/SeeGalleryViewModel.kt
+++ AndibagProject/app/src/main/java/com/example/andibagproject/feature/gallery/viewmodel/SeeGalleryViewModel.kt
@@ -1,9 +1,29 @@
 package com.example.andibagproject.feature.gallery.viewmodel
 
+import android.content.ContentValues.TAG
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.andibagproject.data.gallery.SeeGalleryRepository
 
 class SeeGalleryViewModel(
     private val rp : SeeGalleryRepository
 ):ViewModel() {
+
+    val success: MutableLiveData<Boolean> = MutableLiveData()
+    val fail: MutableLiveData<Int> = MutableLiveData()
+
+    fun seeGallery(){
+        rp.seeGallery()
+            .doOnError { throwable->
+                Log.d(TAG, "Error: $throwable")
+            }
+            .subscribe { response->
+                if(response.isSuccessful){
+                    success.value = true
+                }else{
+                    fail.value = response.code()
+                }
+            }
+    }
 }
