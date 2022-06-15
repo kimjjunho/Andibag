419405ff739b6192d54f6e235cc1956a994b5f6c
kimjjunho <kimjjun1007@gmail.com>
2022-06-16 00:24:28 +0900

    init::BaseActivity

---
+++ AndibagProject/app/src/main/java/com/example/andibagproject/feature/base/BaseActivity.kt
@@ -0,0 +1,32 @@
+package com.example.andibagproject.feature.base
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.annotation.LayoutRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+
+abstract class BaseActivity<B: ViewDataBinding>(
+    @LayoutRes private val layoutId: Int
+): AppCompatActivity() {
+
+    protected lateinit var binding: B
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = DataBindingUtil.setContentView(this,layoutId)
+        binding.lifecycleOwner = this
+
+    }
+
+    fun showToastLong(msg:String){
+        Toast.makeText(this,msg, Toast.LENGTH_LONG).show()
+    }
+    fun showToastShort(msg:String){
+        Toast.makeText(this,msg, Toast.LENGTH_SHORT).show()
+    }
+
+    abstract fun observeEvent()
+}
