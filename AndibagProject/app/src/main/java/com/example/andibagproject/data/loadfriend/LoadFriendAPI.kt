48e355f6fbdbc99caa7ed74f76acd85d3bd2bc52
kimjjunho <kimjjun1007@gmail.com>
2022-06-17 16:55:02 +0900

    init::LoadFriend

--- AndibagProject/app/src/main/java/com/example/andibagproject/data/loadfriend/LoadFriendAPI.kt
+++ AndibagProject/app/src/main/java/com/example/andibagproject/data/loadfriend/LoadFriendAPI.kt
@@ -1,13 +1,13 @@
 package com.example.andibagproject.data.loadfriend
 
 import com.example.andibagproject.feature.friend.load.model.LoadFriendResponse
-import io.reactivex.Single
+import io.reactivex.rxjava3.core.Single
 import retrofit2.Response
-import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.POST
 
 interface LoadFriendAPI {
-    @GET
+    @POST("friend")
     fun loadFriend(
         @Header("Authorization") header: String
     ): Single<Response<LoadFriendResponse>>
--- AndibagProject/app/src/main/java/com/example/andibagproject/data/loadfriend/LoadFriendRepository.kt
+++ AndibagProject/app/src/main/java/com/example/andibagproject/data/loadfriend/LoadFriendRepository.kt
@@ -3,12 +3,15 @@ package com.example.andibagproject.data.loadfriend
 import com.example.andibagproject.ACCESS_TOKEN
 import com.example.andibagproject.data.loadFriendAPI
 import com.example.andibagproject.feature.friend.load.model.LoadFriendResponse
-import io.reactivex.Single
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.annotations.NonNull
 import io.reactivex.rxjava3.schedulers.Schedulers
 import retrofit2.Response
+import io.reactivex.rxjava3.core.Single
 
 class LoadFriendRepository {
-    fun loadFriend() : Single<Response<LoadFriendResponse>> =
+    fun loadFriend() : @NonNull Single<Response<LoadFriendResponse>> =
         loadFriendAPI.loadFriend(ACCESS_TOKEN)
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeOn(Schedulers.io())
 }
