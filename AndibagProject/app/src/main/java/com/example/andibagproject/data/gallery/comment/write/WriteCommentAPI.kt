5f515efd42757c6f02e48fbc25988ce779be2dd3
kimjjunho <kimjjun1007@gmail.com>
2022-06-23 11:54:08 +0900

    init::WriteCommentAPI

---
+++ AndibagProject/app/src/main/java/com/example/andibagproject/data/gallery/comment/write/WriteCommentAPI.kt
@@ -0,0 +1,15 @@
+package com.example.andibagproject.data.gallery.comment.write
+
+import retrofit2.http.Body
+import retrofit2.http.Header
+import retrofit2.http.POST
+import retrofit2.http.Path
+
+interface WriteCommentAPI {
+    @POST("comment/{id}")
+    fun writeComment(
+        @Header("Authorization") header: String,
+        @Body content: String,
+        @Path("id") id: Int
+    )
+}
