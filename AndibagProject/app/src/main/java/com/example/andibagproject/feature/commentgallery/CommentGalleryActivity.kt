410715153a0e5130be978ba735b41925e07f0e05
kimjjunho <kimjjun1007@gmail.com>
2022-06-24 11:26:45 +0900

    init::CommentGalleryActivity

---
+++ AndibagProject/app/src/main/java/com/example/andibagproject/feature/commentgallery/CommentGalleryActivity.kt
@@ -0,0 +1,12 @@
+package com.example.andibagproject.feature.commentgallery
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.andibagproject.R
+
+class CommentGalleryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_comment_gallery)
+    }
+}
