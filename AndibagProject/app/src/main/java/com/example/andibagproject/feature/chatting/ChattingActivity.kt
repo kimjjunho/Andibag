5ff4a1e63b4835464ed490395d5a255f9f634167
kimjjunho <kimjjun1007@gmail.com>
2022-07-01 14:03:35 +0900

    init::ChattingActivity View

---
+++ AndibagProject/app/src/main/java/com/example/andibagproject/feature/chatting/ChattingActivity.kt
@@ -0,0 +1,16 @@
+package com.example.andibagproject.feature.chatting
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.andibagproject.R
+import com.example.andibagproject.databinding.ActivityChattingBinding
+import com.example.andibagproject.feature.base.BaseActivity
+
+class ChattingActivity : BaseActivity<ActivityChattingBinding>(R.layout.activity_chatting) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun observeEvent() {}
+}
