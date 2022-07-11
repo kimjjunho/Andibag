1a27f39d22f2be09752f64f2604a0f223e3cc0cc
kimjjunho <kimjjun1007@gmail.com>
2022-07-11 14:30:04 +0900

    init::LoadImagegAddressRepository

---
+++ AndibagProject/app/src/main/java/com/example/andibagproject/data/image/LoadImageAddressRepository.kt
@@ -0,0 +1,18 @@
+package com.example.andibagproject.data.image
+
+import com.example.andibagproject.data.loadImageAddressAPI
+import com.example.andibagproject.feature.image.LoadImageAddressRequest
+import com.example.andibagproject.feature.image.LoadImageAddressResponse
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.annotations.NonNull
+import io.reactivex.rxjava3.core.Single
+import io.reactivex.rxjava3.schedulers.Schedulers
+import okhttp3.MultipartBody
+import retrofit2.Response
+
+class LoadImageAddressRepository {
+    fun loadImageAddress(loadImageAddressRequest: LoadImageAddressRequest) : @NonNull Single<Response<LoadImageAddressResponse>> =
+        loadImageAddressAPI.loadImageAddress(loadImageAddressRequest)
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeOn(Schedulers.io())
+}
